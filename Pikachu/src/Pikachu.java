9b858b6738014934d766b4eb333676e23dcc108a
conanlee718 <17leec92@millshighsch.org>
2016-08-29 09:45:14 -0700

    Pikachu code
    Pikachu

---
+++ Pikachu/src/Pikachu.java
@@ -0,0 +1,9 @@
+
+public class Pikachu {
+	public static void main(String[] args){
+		System.out.println("\t\t   (\\__/_)");
+		System.out.println("\t\t   (o^.^)");
+		System.out.println("\t\tz(_(\")(\")");
+	}
+
+}
