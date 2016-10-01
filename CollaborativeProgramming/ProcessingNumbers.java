1aaac2dcd41bb340b93992bfd48d28fa23807023
conanlee718 <17leec92@millshighsch.org>
2016-10-01 13:16:37 -0700

    10/1

--- CollaborativeProgramming/ProcessingNumbers.java
+++ CollaborativeProgramming/ProcessingNumbers.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class ProcessingNumbers {
 	
-	public static void main(String[] args){
+	public static int Processnum(1, 2, 3, 4, 5, 6, 7, 8, 9, 10){
 	Scanner userInput= new Scanner(System.in);
 	System.out.println("Input 10 integers!");
 	int min=userInput.nextInt();
@@ -12,7 +12,23 @@ public class ProcessingNumbers {
 	int largestEven=0;
 	
 	for(int i=1;i<10;i++){
-		if(min%2==0){
-			
+		if(inputNumber%2==0){
+			if(inputNumber<min){
+				inputNumber=min;
+			} else { 
+				inputNumber=largestEven;
+				
+			}
+			evenSum+=inputNumber;
+		} else {
+			if(inputNumber<min){
+				inputNumber=min;
+			} else {
+				inputNumber=max;
+			}
 		}
 	}
+	System.out.println("Smallest number is "+min+"and largest number is "+max);
+	System.out.println("Sum of even numbers is "+evenSum+"and largest even is "+largestEven);
+	
+}
