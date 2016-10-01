d9a6aba88743d6ef61a7fd49acd5fe79637f8f93
conanlee718 <17leec92@millshighsch.org>
2016-09-30 22:29:10 -0700

    9/30 everything is restored-- just need to work on collaborativecode

---
+++ CollaborativeProgramming/ProcessingNumbers.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class ProcessingNumbers {
+	
+	public static void main(String[] args){
+	Scanner userInput= new Scanner(System.in);
+	System.out.println("Input 10 integers!");
+	int min=userInput.nextInt();
+	int max=min;
+	int inputNumber=min;
+	int evenSum=0;
+	int largestEven=0;
+	
+	for(int i=1;i<10;i++){
+		if(min%2==0){
+			
+		}
+	}
