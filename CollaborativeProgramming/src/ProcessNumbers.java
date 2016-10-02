bddc95f5455874c60deef80ac238ea8a205ad5a5
conanlee718 <17leec92@millshighsch.org>
2016-10-01 23:05:02 -0700

    10/1

---
+++ CollaborativeProgramming/src/ProcessNumbers.java
@@ -0,0 +1,42 @@
+//Author: Conan Lee APCS 2nd period
+//Date started:9/28
+//What we want:
+// 1) Accepts as user input a string of numbers.
+// 2) Prints the smallest and largest of all numbers supplied by user.
+// 3) Prints the sum of all even numbers, along with largest even number typed.
+
+import java.util.Scanner;
+
+public class ProcessNumbers {
+		
+	public static void main(String[] args){
+	Scanner userInput= new Scanner(System.in);
+	System.out.println("Input 10 integers!");
+	int min=userInput.nextInt();
+	int max=min;
+	int inputNumber=min;
+	int evenSum=0;
+	int largestEven=0;
+	
+	for(int i=1;i<10;i++){
+		inputNumber=userInput.nextInt();
+		if(inputNumber<=min){
+			min=inputNumber;
+		} else if(inputNumber>=max){
+			max=inputNumber;
+		}
+		if(inputNumber%2==0){
+			evenSum+=inputNumber;
+			if(inputNumber>largestEven){
+				largestEven=inputNumber;
+			}
+		}
+	}
+		
+	System.out.println("Smallest number is "+min+" and largest number is "+max+".");
+	System.out.println("Sum of even numbers is "+evenSum+" and largest even is "+largestEven+".");
+	userInput.close();
+	}
+}
+	
+
